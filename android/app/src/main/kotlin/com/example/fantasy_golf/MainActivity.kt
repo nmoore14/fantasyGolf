bcfbcc30a26964ab441fe1b82ed66919009fe7e0
Nick Moore <nmoore@foreup.com>
2023-05-14 00:50:54 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/fantasy_golf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fantasy_golf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fantasy_golf/components/nav/bottom_nav.dart';
+import 'package:fantasy_golf/components/nav/side_rail.dart';
+
+void main() => runApp(const FantasyGolfApp());
+
+class FantasyGolfApp extends StatelessWidget {
+  const FantasyGolfApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+    MediaQueryData queryData;
+    queryData = MediaQuery.of(context);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: queryData.size.width < 768.0 ? const BottomNav() : const SideRail(),
+    );
+  }
+}
+
+
